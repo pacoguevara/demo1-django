104b64484f42b1e74051209b11b6592760e273f8
Paco Guevara <pacoguevaraa@gmail.com>
2016-02-17 21:33:08 -0600

    using templates to render views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def hello_world(request):
+    product = Product.objects.order_by('id')
+    template = loader.get_template('index.html')
+    context = {
+        'product': product
+    }
+    return HttpResponse(template.render(context, request))
