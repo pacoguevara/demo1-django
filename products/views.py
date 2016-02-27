58f1c98be5456ae418c2928c7fea64e2280cbf64
Paco Guevara <pacoguevaraa@gmail.com>
2016-02-27 13:04:42 -0600

    add form to create new products, but image upload not working

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from .forms import ProductForm
 
 def hello_world(request):
     product = Product.objects.order_by('id')
@@ -16,5 +17,22 @@ def product_detail(request, pk):
     template = loader.get_template('product_detail.html')
     context = {
         'product': product
+    }
+    return HttpResponse(template.render(context, request))
+
+def new_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            product = form.save()
+            product.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = ProductForm()
+
+
+    template = loader.get_template('new_product.html')
+    context = {
+        'form': form
     }
     return HttpResponse(template.render(context, request))
