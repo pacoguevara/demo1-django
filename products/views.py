5a502ffd21adc528bd3a50c8a952a8ca10e5b51e
Paco Guevara <pacoguevaraa@gmail.com>
2016-02-28 22:44:12 -0600

    upload image to products on create

--- products/views.py
+++ products/views.py
@@ -22,7 +22,7 @@ def product_detail(request, pk):
 
 def new_product(request):
     if request.method == 'POST':
-        form = ProductForm(request.POST)
+        form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             product = form.save()
             product.save()
