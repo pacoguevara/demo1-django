5e4d2a41f0ab7a511c0a8d494145c9d9ecbdfc82
Paco Guevara <pacoguevaraa@gmail.com>
2016-02-29 22:00:15 -0600

    create associations between two class models

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Favorite
 # Register your models here.
 # admin.site.register(Product)
 @admin.register(Product)
 class AdminProduct(admin.ModelAdmin):
     list_display = ('id', 'name', 'category', 'description', 'price')
     list_filter = ('category', )
+
+@admin.register(Favorite)
+class AdminFavorite(admin.ModelAdmin):
+    list_display = ('user', 'product', )
+    list_filter = ('user', 'product', )
